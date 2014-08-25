af00c736cd047a8f98c5551a885f9312f3bca21e
frontainer <frontainer@gmail.com>
2014-08-25 21:19:10 +0900

    不具合対応でfrontnoteバージョンアップ

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ var gulp = require('gulp');
 var gsg = require('./index');
 
 gulp.task('default', function() {
-    gulp.src('../frontnote/test/less/**/*')
+    gulp.src('test/**/*.scss')
         .pipe(gsg({
+            out: './docs',
             cwd: '../frontnote',
             verbose: false,
             clean: true
