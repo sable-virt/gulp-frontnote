57324f921cff0dae823af278fb4b1aee85e4cdf3
frontainer <frontainer@gmail.com>
2015-04-15 15:46:23 +0900

    FrontNote v1.0.0に合わせて修正

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 var through = require('through2'),
-    path = require('path'),
-    styleGuide = require('frontnote');
+    FrontNote = require('frontnote');
 
 /**
  * プラグイン関数
  * @param options
  * @returns {*}
  */
-function FrontNote(options) {
+function gulpFrontNote(options) {
     var files = [];
+    var note = new FrontNote(options);
     var stream = through.obj(function(file,encoding, callback) {
-        this.push(file);
-        return callback();
-    });
-    stream.on('data', function(file) {
         if (file.isBuffer()) {
             files.push(file.path);
         }
-    });
-    stream.on('end', function() {
-        styleGuide(files,options);
+        this.push(file);
+        callback();
+    },function(callback) {
+        note.render(files, callback);
     });
     return stream;
 }
 // プラグイン関数をエクスポート
-module.exports = FrontNote;
+module.exports = gulpFrontNote;
